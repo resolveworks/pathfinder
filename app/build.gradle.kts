6ad82fcc74103989ed02441c0158342facfb096d
Johan Schuijt <johan@resolve.works>
2026-09-22 16:26:11 +0200

    Release v0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,8 +93,8 @@ android {
         applicationId = "works.resolve.pathfinder"
         minSdk = 34
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     signingConfigs {
